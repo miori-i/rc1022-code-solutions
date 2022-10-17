6e912021dfa22eb627ad43479a04a7c4d6a277c6
Miori Imai <imamio1227@gmail.com>
2022-10-17 17:10:32 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Miori Imai';
+console.log('value of fullName: ', fullName);
+var isCool = true;
+console.log('value of isCool: ', isCool);
+var totalPets = 2;
+console.log('value of totalPets: ', totalPets);
+
+console.log('typeof fullName:', typeof 'Miori Imai');
+console.log('typeof isCool:', typeof true);
+console.log('typeof totalPets:', typeof 2);
+
+var unknown = null;
+console.log('value of unknown: ', unknown);
+console.log('typeof unknown: ', typeof null);
+
+var never;
+console.log('value of never: ', never);
+console.log('typeof never: ', typeof undeclaredVariable);
