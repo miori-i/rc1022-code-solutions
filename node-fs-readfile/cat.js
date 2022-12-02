540fccb4d254bebd6f88ca5e7a5288d32a5675e1
Miori Imai <imamio1227@gmail.com>
2022-12-02 03:50:09 +0000

    Completed level 3.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,60 +1,36 @@
 const fs = require('fs');
 
-// fs.readFile(process.argv[2], 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-
-//   fs.readFile(process.argv[3], 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-
-//     fs.readFile(process.argv[4], 'utf8', (err, data) => {
-//       if (err) throw err;
-//       console.log(data);
-//     });
-//   });
-// });
-
-// let count = 2;
-// fs.readFile(process.argv[count], 'utf8', (err, data) => {
-//   if (err) {
-//     throw err;
-//   } else if (process.argv.length === 1) {
-//     console.log(data);
-//   } else if (count <= process.argv.length) {
-//     fs.readFile(process.argv[count], 'utf8', (err, data) => {
-//       if (err) throw err;
-//       console.log(data);
-
-//     });
-//     count++;
-//   }
-
-// });
-var array = [];
-
-for (var i = 2; i < process.argv.length; i++) {
-  read(i);
-  console.log(array.length);
-}
+var index = 2;
 
 function read(index) {
-
-  array.push(
+  if (index < process.argv.length) {
     fs.readFile(process.argv[index], 'utf8', (err, data) => {
       if (err) throw err;
       console.log(data);
-    })
-  );
-  return array;
+      index++;
+      read(index);
+    });
+  }
+
 }
 
-// for (var i = 2; i < process.argv.length; i++) {
-//   var array = [];
-//   fs.readFile(process.argv[i], 'utf8', (err, data) => {
-//     if (err) throw err;
-//     // console.log(data);
-//     array.push(data);
-//   });
-//   console.log('array:', array);
-// }
+read(index);
+
+/*
+
+// recursive ベースケースを持つ、他の方法
+
+function read(index) {
+  if (index === process.argv.length) { //base case
+    return;
+  }
+
+  fs.readFile(process.argv[index], 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    index++;
+    read(index);
+  });
+}
+
+*/
