da220be182b29b5b30f4598886389be1a418268e
Miori Imai <imamio1227@gmail.com>
2022-12-08 01:37:56 +0000

    Completed sql-select exercise.

---
+++ sql-select/expensive-films.sql
@@ -0,0 +1,17 @@
+select "filmId",
+       "title",
+       "description",
+       "releaseYear",
+       "languageId",
+       "originalLanguageId",
+       "rentalDuration",
+       "rentalRate",
+       "length",
+       "replacementCost",
+       "rating",
+       "updatedAt",
+       "specialFeatures",
+       "fulltext"
+  from "films"
+  order by "replacementCost" desc
+  limit 10;
