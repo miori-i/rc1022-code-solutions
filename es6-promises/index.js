a6daca5be4ec2de993ddc4fe3e819e7bca193d0c
Miori Imai <imamio1227@gmail.com>
2022-12-09 20:10:27 +0000

    Add a new line.

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,9 +2,10 @@ const takeAChance = require('./take-a-chance');
 
 const result = takeAChance('Miori');
 
-result.then(value => {
-  console.log(value);
-})
+result
+  .then(value => {
+    console.log(value);
+  })
   .catch(error => {
     console.error(error.message);
   });
