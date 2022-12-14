540e4ac554a1fdd55b96463aa2a9984e89eb26b2
Miori Imai <imamio1227@gmail.com>
2022-12-14 20:02:47 +0000

    Completed react-class-components exercise.

---
+++ react-class-components/src/index.jsx
@@ -0,0 +1,50 @@
+// refactor CustomButton from a function component to a class component.
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function CustomButton(props) {
+  return (
+    <button>{props.text}</button>
+  );
+}
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class CustomButton extends React.Component {
+  render() {
+
+    return (
+      <button>{this.props.text}</button>
+    );
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
