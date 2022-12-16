1ad7477c353abd0a2cc419b6ddbd6638c1b627a1
Miori Imai <imamio1227@gmail.com>
2022-12-16 18:05:23 +0000

    Completed react-rendering-lists exercise.

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function PokemonList(props) {
+  const pokemons = props.pokedex;
+  const listItems = pokemons.map(pokemon =>
+    <li key={pokemon.name}>{pokemon.name}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<PokemonList pokedex={pokedex} />);
